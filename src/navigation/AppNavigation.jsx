cc9d66a4e2ec7d836df76cfba6606505867af448
Radoslav Nikolaev Yordanov <radoslavyordanov1415@gmail.com>
2026-02-10 19:19:12 +0100

    Add React Navigation

---
+++ src/navigation/AppNavigation.jsx
@@ -0,0 +1,12 @@
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/auth/LoginScreen";
+
+export default function AppNavigation() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen}/>
+    </Stack.Navigator>
+  );
+}
